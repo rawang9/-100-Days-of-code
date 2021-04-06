745d0de80b38c482bf0fef9b30c45fff859d3fdd
Akarshit Gupta <81812083+rawang9@users.noreply.github.com>
2021-04-07 04:59:50 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,36 @@
+=============================================================================
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+print("helo\nhello")#use of \n
+#debug 
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+use of len to get text size=============================================================================
+print(len(input("What is your name?")))
+🚨 Don't change the code below 👇
+=============================================================================
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+c=a
+a=b
+b=c
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+=============================================================================
+print("Welcome to the band Name Generator.")
+city_name=input("Whats's name of city you grew up in?\n")
+pet_name=input("Whats's your pets's name?\n")
+print("Your band name could be "+city_name+ ' ' +pet_name)
