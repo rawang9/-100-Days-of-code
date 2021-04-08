7e549c990ed1cf25d62b4b1a9607fa3aabd41582
Akarshit Gupta <81812083+rawang9@users.noreply.github.com>
2021-04-09 03:34:26 +0530

    Create day3.py

---
+++ day3.py
@@ -0,0 +1,41 @@
+#=============================================================================
+# =============================================================================
+print("Welcome to the love calculator")
+name1=input("enter you name ")
+name2=input("enter her name ")
+l=name1.lower().count('l')
+o=name1.lower().count('o')
+v=name1.lower().count('v')
+e=name1.lower().count('e')
+
+t=name2.lower().count('t')
+r=name2.lower().count('r')
+u=name2.lower().count('u')
+e=name2.lower().count('e')
+boy=t+r+u+e
+grl=l+o+v+e
+lov=int(str(boy)+str(grl))
+print(lov)
+if lov<=10 or lov>90:
+    print(f"your score is {lov} , you go together like coke and mintos")
+if lov>40 and lov<50:
+    print(f"your score is {lov} , you are alright")
+else:
+    print("you are all right")
+
+# main project the tresure hunt game =============================================================================
+print("Welcome to Tresure island,\n Your mission is to find the tresure")
+step1=input("You're at a cross road,Where do you want to go? Type 'left' or 'right' \n")
+if step1=="left":
+    step2=input("you came on a middle of a lake you like to 'wait' for boat or like to 'swim' \n")
+    if step2=='wait':
+        step3=input("you arrive at islan,there is a house with one 'Red' door,'Yellow' and one 'Blue door' \n")
+        if step3=='Yellow':
+            print("Hurray!  you won the game and tresure")
+        else:
+            print("there is a lion ")
+    else:
+        print("shark will eat you fool ")
+else:
+    print("killed by a car ")
+#=============================================================================
